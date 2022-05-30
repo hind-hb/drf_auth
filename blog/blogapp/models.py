29239284be71c712f0ae432c149b2625d7c4832f
hind-hb <hind.hbashenh@gmail.com>
2022-05-31 01:40:34 +0300

    add jwt

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Party(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    # user = models.ForeignKey(get_user_model())
+    def __str__(self):
+        return self.title
